dd74d16636e2502de7d4eebdeeec35c3451a447e
erlandoss <32767083+erlandoss@users.noreply.github.com>
2019-11-02 21:34:36 +0100

    statuscake test4

--- statuscake.tf
+++ statuscake.tf
@@ -1,15 +1,6 @@
-resource "statuscake_test" "development" {
-  website_name = "app-development"
-  website_url = "${heroku_app.development.web_url}"
-  test_type = "HTTP"
-  check_rate = "300"
-  contact_group = ["Default Contact Group"]
-
-}
-
-resource "statuscake_test" "stage" {
-  website_name = "app-stage"
-  website_url = "${heroku_app.stage.web_url}"
+resource "statuscake_test" "staging" {
+  website_name = "app-staging"
+  website_url = "${heroku_app.staging.web_url}"
   test_type = "HTTP"
   check_rate = "300"
   contact_group = ["Default Contact Group"]
@@ -17,10 +8,20 @@ resource "statuscake_test" "stage" {
 }
 
 resource "statuscake_test" "production" {
-   website_name = "app-production"
-   website_url = "${heroku_app.production.web_url}"
-   test_type = "HTTP"
-   check_rate = "300"
-   contact_group = ["Default Contact Group"]
+  website_name = "app-production"
+  website_url = "${heroku_app.production.web_url}"
+  test_type = "HTTP"
+  check_rate = "300"
+  contact_group = ["Default Contact Group"]
 
- }
+}
+
+resource "statuscake_test" "ci" {
+  website_name = "app-ci"
+  website_url = "${heroku_app.ci.web_url}"
+  test_type = "HTTP"
+  check_rate = "300"
+  contact_group = ["Default Contact Group"]
+
+}
+
