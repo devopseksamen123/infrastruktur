c4f382895681e2bd8fa17b7c2629d4bcdd22a469
erlandoss <32767083+erlandoss@users.noreply.github.com>
2019-11-02 21:30:34 +0100

    fail

--- statuscake.tf
+++ statuscake.tf
@@ -1,3 +1,12 @@
+resource "statuscake_test" "development" {
+  website_name = "app-development"
+  website_url = "${heroku_app.development.web_url}"
+  test_type = "HTTP"
+  check_rate = "300"
+  contact_group = ["Default Contact Group"]
+
+}
+
 resource "statuscake_test" "stage" {
   website_name = "app-stage"
   website_url = "${heroku_app.stage.web_url}"
@@ -8,8 +17,8 @@ resource "statuscake_test" "stage" {
 }
 
 resource "statuscake_test" "production" {
-   website_name = "app-prod"
-   website_url = "${heroku_app.prod.web_url}"
+   website_name = "app-production"
+   website_url = "${heroku_app.production.web_url}"
    test_type = "HTTP"
    check_rate = "300"
    contact_group = ["Default Contact Group"]
