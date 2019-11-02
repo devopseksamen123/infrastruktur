c5a3ce2fd5743f99ef73a11bad65eb78b2aebca7
erlandoss <32767083+erlandoss@users.noreply.github.com>
2019-11-02 21:12:42 +0100

    statuscake test2

--- statuscake.tf
+++ statuscake.tf
@@ -1,24 +1,24 @@
 resource "statuscake_test" "development" {
-  website_name = "app-development"
-  website_url = "${heroku_app.development.web_url}"
+  website_name = "app-dev"
+  website_url = "${heroku_app.dev.web_url}"
   test_type = "HTTP"
   check_rate = "300"
   contact_group = ["Default Contact Group"]
 
 }
 
-resource "statuscake_test" "stage" {
-  website_name = "app-stage"
-  website_url = "${heroku_app.stage.web_url}"
+resource "statuscake_test" "stag" {
+  website_name = "app-stag"
+  website_url = "${heroku_app.stag.web_url}"
   test_type = "HTTP"
   check_rate = "300"
   contact_group = ["Default Contact Group"]
 
 }
 
-resource "statuscake_test" "production" {
-   website_name = "app-production"
-   website_url = "${heroku_app.production.web_url}"
+resource "statuscake_test" "prod" {
+   website_name = "app-prod"
+   website_url = "${heroku_app.prod.web_url}"
    test_type = "HTTP"
    check_rate = "300"
    contact_group = ["Default Contact Group"]
